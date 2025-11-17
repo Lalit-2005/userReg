963a28f4f6cc48fb422e4c56b1f7daa04c7b2dfb
Lalit Chauhan <lalit.chauhan@3pillarglobal.com>
2025-11-17 13:38:16 +0530

    Added CORS config

--- UserReg/backend/src/main/java/com/userpage/UserReg/controller/EmployeeController.java
+++ UserReg/backend/src/main/java/com/userpage/UserReg/controller/EmployeeController.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
-@CrossOrigin(origins = "http://localhost:5173")  // React frontend
+@CrossOrigin(origins = "https://user-reg-lyart.vercel.app")
+  // React frontend
 public class EmployeeController {
 
     @Autowired
